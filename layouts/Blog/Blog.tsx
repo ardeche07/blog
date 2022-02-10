65991b5db827d70dbd9525e3e6a38abb2f161163
Katrin Shibakova <katrina.gubanova@gmail.com>
2022-02-10 17:47:44 +0300

    fixed links in the Pagination component

--- layouts/Blog/Blog.tsx
+++ layouts/Blog/Blog.tsx
@@ -89,7 +89,7 @@ function BlogIndexPage({
                 <Pagination
                   currentPage={currentPage}
                   maxPage={maxPages}
-                  baseUrl={!!tag ? `/tags/${tag}` : "/"}
+                  baseUrl={!!tag ? `/tags/${tag}` : ""}
                 />
               )}
             </Box>
